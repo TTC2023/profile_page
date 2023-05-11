f8db3b4b5c04920f2b4d7d180dfc24f068dd9c27
Tyler Correll <corrett10@gmail.com>
2023-05-11 10:29:45 -0600

    just needs more editing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,32 @@
-import { motion } from 'framer-motion'
-import { styles } from '../styles'
-import { ComputersCanvas } from './canvas'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
+
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Tyler</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Tyler</span>
+          </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          Add Later!
+          React front-end developer creating engaging UIs with HTML, <br className='sm:block hidden' />
+          CSS, JavaScript, and the power of React.js.
           </p>
         </div>
-        </div>
-      <ComputersCanvas/>
+      </div>
+
+      <ComputersCanvas />
+
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
@@ -36,7 +45,7 @@ const Hero = () => {
         </a>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
